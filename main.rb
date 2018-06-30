da5ae4b9b256fc5eb54ab1582ace8e013405605e
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2018-07-01 01:19:42 +0900

    Initialize

---
+++ main.rb
@@ -0,0 +1,30 @@
+require 'sinatra'
+
+get '/' do
+  <<~HTML
+    <style>
+      body {
+        text-align: center;
+        display: table;
+        width: 100%;
+        height: 100%;
+      }
+      h1 {
+        font-size: 5rem;
+        display: table-cell;
+        vertical-align: middle;
+      }
+    </style>
+    <body>
+      <h1>
+        #{
+          [
+            '働いたら負け',
+            'YOU SHOULD NOT WORK!',
+            '労働は悪',
+          ].sample
+        }
+      </h1>
+    </body>
+  HTML
+end
